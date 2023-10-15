d68c62c48ff64fae68caf264d044c73eacb20d31
root <esllam.saeid@gmail.com>
2023-10-15 05:19:21 -0700

    state.py

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module contains the definition for the 'State' Class """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class Definition for State Class, Subclass of BaseModel """
+
+    name = ""
