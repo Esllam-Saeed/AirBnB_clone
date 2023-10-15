fbac9aea21164c9a3a78eca08981d816334d7937
root <esllam.saeid@gmail.com>
2023-10-15 06:56:42 -0700

    delete all

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" This module contains the definition for the 'Amenity' Class """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ Class Definition for Amenity Class, Subclass of BaseModel """
-
-    name = ""
--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" This module contains the definition for the 'State' Class """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ Class Definition for State Class, Subclass of BaseModel """
-
-    name = ""
