599fe81a4f55a4e71f7aeb504a00cbeecdc06849
root <esllam.saeid@gmail.com>
2023-10-15 05:15:09 -0700

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module contains the definition for the 'Amenity' Class """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class Definition for Amenity Class, Subclass of BaseModel """
+
+    name = ""
